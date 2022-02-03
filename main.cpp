51cb0d724cccb01698c7c12c861bccc41a67f6f7
sekine104 <sekine104@outlook.jp>
2022-02-03 11:46:55 +0900

    update test

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "source.h"
+using namespace std;
+
+
+int start_menu(void) {
+    int mode;
+    cout << "SimpleRPG!\n";
+    cout << "[1]Start [0]Exit > ";
+    cin >> mode;
+    return mode;
+}
+
+int main() {
+    int mainloop;
+    cout << "Hello World!\n";
+    mainloop = start_menu();
+    hello();
+}
